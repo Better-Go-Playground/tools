15c38d41647f443813d441957c8e56e2563ff911
x1unix <9203548+x1unix@users.noreply.github.com>
2026-01-25 17:45:10 -0500

    feat: split entrypoint for wasm
    Signed-off-by: x1unix <9203548+x1unix@users.noreply.github.com>

--- gopls/main_wasm.go
+++ gopls/main_wasm.go
@@ -1,37 +1,27 @@
-// Copyright 2019 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Gopls (pronounced “go please”) is an LSP server for Go.
-// The Language Server Protocol allows any text editor
-// to be extended with IDE-like features;
-// see https://langserver.org/ for details.
-//
-// See https://go.dev/gopls for comprehensive documentation on Gopls.
 package main
 
 import (
 	"context"
+	"errors"
+	"io"
 	"log"
 	"os"
+	"strconv"
 
-	"golang.org/x/telemetry"
-	"golang.org/x/telemetry/counter"
-	"golang.org/x/tools/gopls/internal/cmd"
+	"golang.org/x/tools/gopls/internal/cache"
+	"golang.org/x/tools/gopls/internal/debug"
 	"golang.org/x/tools/gopls/internal/filecache"
+	"golang.org/x/tools/gopls/internal/lsprpc"
+	"golang.org/x/tools/gopls/internal/protocol"
+	"golang.org/x/tools/gopls/internal/util/fakenet"
 	versionpkg "golang.org/x/tools/gopls/internal/version"
-	"golang.org/x/tools/internal/tool"
+	"golang.org/x/tools/internal/jsonrpc2"
 )
 
 var version = "" // if set by the linker, overrides the gopls version
 
 func main() {
 	versionpkg.VersionOverride = version
-
-	telemetry.Start(telemetry.Config{
-		ReportCrashes: true,
-		Upload:        true,
-	})
 
 	// Force early creation of the filecache and refuse to start
 	// if there were unexpected errors such as ENOSPC. This
@@ -47,10 +37,31 @@ func main() {
 	// either re-create it or just fail the RPC with an
 	// informative error and terminate the process.
 	if _, err := filecache.Get("nonesuch", [32]byte{}); err != nil && err != filecache.ErrNotFound {
-		counter.Inc("gopls/nocache")
 		log.Fatalf("gopls cannot access its persistent index (disk full?): %v", err)
 	}
 
 	ctx := context.Background()
-	tool.Main(ctx, cmd.New(), os.Args[1:])
+	stream := jsonrpc2.NewHeaderStream(fakenet.NewConn("stdio", os.Stdin, os.Stdout))
+	if isTraceEnabled() {
+		stream = protocol.LoggingStream(stream, os.Stderr)
+		ctx = debug.WithInstance(ctx)
+	}
+
+	srv := lsprpc.NewStreamServer(cache.New(nil), true, nil)
+	conn := jsonrpc2.NewConn(stream)
+
+	err := srv.ServeStream(ctx, conn)
+	if err != nil && !errors.Is(err, io.EOF) {
+		log.Fatalf("ServeStream: %s", err)
+	}
+}
+
+func isTraceEnabled() bool {
+	val := os.Getenv("LSP_TRACE")
+	if val == "" {
+		return false
+	}
+
+	v, _ := strconv.ParseBool(val)
+	return v
 }
